f19c657354d4605b580aa52bddf7c9722b6985db
Mariomaster <bent@mail.de>
2015-06-18 16:55:34 +0200

    Initial Commit

---
+++ NSMB Worldmap Editor/WorldmapEditor.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace NSMB_Worldmap_Editor
+{
+    partial class WorldmapEditor
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.worldComboBox = new System.Windows.Forms.ToolStripComboBox();
+            this.Nodes = new System.Windows.Forms.TabPage();
+            this.nodeEditor = new NSMB_Worldmap_Editor.Controls.NodeEditor();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pathEditor1 = new NSMB_Worldmap_Editor.Controls.PathEditor();
+            this.editor = new NSMB_Worldmap_Editor.Controls.EditorControl();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.Nodes.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1247, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Enabled = false;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Enabled = false;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.worldComboBox});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1247, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // worldComboBox
+            // 
+            this.worldComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.worldComboBox.Enabled = false;
+            this.worldComboBox.Items.AddRange(new object[] {
+            "World 1",
+            "World 2",
+            "World 3",
+            "World 4",
+            "World 5",
+            "World 6",
+            "World 7",
+            "World 8"});
+            this.worldComboBox.Name = "worldComboBox";
+            this.worldComboBox.Size = new System.Drawing.Size(121, 25);
+            this.worldComboBox.SelectedIndexChanged += new System.EventHandler(this.worldComboBox_SelectionChangeCommitted);
+            // 
+            // Nodes
+            // 
+            this.Nodes.Controls.Add(this.nodeEditor);
+            this.Nodes.Location = new System.Drawing.Point(4, 22);
+            this.Nodes.Name = "Nodes";
+            this.Nodes.Padding = new System.Windows.Forms.Padding(3);
+            this.Nodes.Size = new System.Drawing.Size(256, 704);
+            this.Nodes.TabIndex = 0;
+            this.Nodes.Text = "Nodes";
+            this.Nodes.UseVisualStyleBackColor = true;
+            // 
+            // nodeEditor
+            // 
+            this.nodeEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nodeEditor.Enabled = false;
+            this.nodeEditor.Location = new System.Drawing.Point(3, 3);
+            this.nodeEditor.Name = "nodeEditor";
+            this.nodeEditor.Size = new System.Drawing.Size(250, 698);
+            this.nodeEditor.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Nodes);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tabControl1.Location = new System.Drawing.Point(0, 49);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(264, 730);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pathEditor1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(256, 704);
+            this.tabPage1.TabIndex = 1;
+            this.tabPage1.Text = "Paths";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pathEditor1
+            // 
+            this.pathEditor1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pathEditor1.Location = new System.Drawing.Point(0, 0);
+            this.pathEditor1.Name = "pathEditor1";
+            this.pathEditor1.Size = new System.Drawing.Size(256, 704);
+            this.pathEditor1.TabIndex = 0;
+            // 
+            // editor
+            // 
+            this.editor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editor.Location = new System.Drawing.Point(264, 49);
+            this.editor.Name = "editor";
+            this.editor.Size = new System.Drawing.Size(983, 730);
+            this.editor.TabIndex = 3;
+            // 
+            // WorldmapEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 779);
+            this.Controls.Add(this.editor);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(600, 570);
+            this.Name = "WorldmapEditor";
+            this.Text = "NSMB Worldmap Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.Nodes.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private Controls.EditorControl editor;
+        private System.Windows.Forms.TabPage Nodes;
+        private Controls.NodeEditor nodeEditor;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ToolStripComboBox worldComboBox;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TabPage tabPage1;
+        private Controls.PathEditor pathEditor1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+
+
+
+
+
+    }
+}
+
---
+++ NSMB Worldmap Editor/WorldmapEditor.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace NSMB_Worldmap_Editor
+{
+    public partial class WorldmapEditor : Form
+    {
+        private int[] dataNodesWorldOffsets, visibleNodesWorldOffsets;
+        int[] nodesPerWorld;
+
+        public List<List<visibleNode>> visibleNodeList;
+        public List<List<dataNode>> dataNodeList;
+
+        public string overlayPath;
+
+        public WorldmapEditor()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            loadXmlFiles();
+
+            nodeEditor.ec = editor;
+            editor.ne = nodeEditor;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!nodeEditor.Enabled) loadOverlay();
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show("There are unsaved changes.\nIf you close the editor without saving, you will lose them.\nDo you want to save?", "NSMB Worldmap Editor", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button3);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    save(false);
+                    loadOverlay();
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    loadOverlay();
+                }
+            }      
+        }
+
+        private void loadOverlay()
+        {
+            long overlayLength = 0;
+
+            OpenFileDialog openOverlayDialog = new OpenFileDialog();
+            openOverlayDialog.InitialDirectory = "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}";
+            openOverlayDialog.FileName = "overlay9_8.bin";
+            openOverlayDialog.Filter = "Binary Files|*.bin|All Files|*.*";
+            openOverlayDialog.Title = "Save overlay9_8";
+
+
+            DialogResult openOverlayDialogResult = openOverlayDialog.ShowDialog();
+            overlayPath = openOverlayDialog.FileName;
+
+            try
+            {
+                overlayLength = new FileInfo(overlayPath).Length;
+            }
+            catch { }
+
+            if (openOverlayDialogResult == DialogResult.OK && overlayLength == 139360)
+            {
+                try
+                {
+                    try
+                    {
+                        Directory.CreateDirectory("Backup");
+                    }
+                    catch { }
+
+                    File.Copy(overlayPath, "Backup/overlay9_8.tmp", true);
+                    readOverlay();
+                }
+                catch
+                {
+                    MessageBox.Show("Temping Overlay failed!", "Error");
+                    Environment.Exit(0);
+                }
+            }
+            else if (overlayLength == 0) { }
+            else
+            {
+                MessageBox.Show("Your file has a wrong size!", "Error");
+            }            
+        }
+
+        private void readOverlay()
+        {
+            visibleNodeList = new List<List<visibleNode>>();
+            dataNodeList = new List<List<dataNode>>();
+
+            //Read Visible Nodes
+            BinaryReader brVN = new BinaryReader(File.Open("Backup/overlay9_8.tmp", FileMode.Open));
+            for (int i = 0; i < 8; i++)
+            {
+                List<visibleNode> subVisibleNodeList = new List<visibleNode>();
+                brVN.BaseStream.Position = visibleNodesWorldOffsets[i];
+                for (int j = 0; j < nodesPerWorld[i]; j++)
+                {
+                    int x, y, z, p1, p2, p3, p4, sp1, sp2, sp3, sp4, c, sc;
+                    p1 = brVN.ReadByte();
+                    p2 = brVN.ReadByte();
+                    p3 = brVN.ReadByte();
+                    p4 = brVN.ReadByte();
+                    sp1 = brVN.ReadByte();
+                    sp2 = brVN.ReadByte();
+                    sp3 = brVN.ReadByte();
+                    sp4 = brVN.ReadByte();
+                    c = brVN.ReadInt16();
+                    sc = brVN.ReadInt16();
+                    x = brVN.ReadInt32();
+                    z = brVN.ReadInt32();
+                    y = brVN.ReadInt32();
+                    subVisibleNodeList.Add(new visibleNode(x, y, z, p1, p2, p3, p4, sp1, sp2, sp3, sp4, c, sc));
+                }
+                visibleNodeList.Add(subVisibleNodeList);
+            }
+            brVN.Close();
+
+            //Read Data Nodes
+            BinaryReader brDN = new BinaryReader(File.Open("Backup/overlay9_8.tmp", FileMode.Open));
+            for (int i = 0; i < 8; i++)
+            {
+                List<dataNode> subDataNodeList = new List<dataNode>();
+                brDN.BaseStream.Position = dataNodesWorldOffsets[i];
+                for (int j = 0; j < nodesPerWorld[i]; j++)
+                {
+                    int area, settings, icon;
+                    byte[] ptr;
+                    ptr = brDN.ReadBytes(4);
+                    area = brDN.ReadByte();
+                    brDN.BaseStream.Position += 2;
+                    icon = brDN.ReadByte();
+                    settings = brDN.ReadByte();
+                    brDN.BaseStream.Position += 3;
+                    subDataNodeList.Add(new dataNode(area, icon, settings, ptr));
+                }
+                dataNodeList.Add(subDataNodeList);
+            }
+            brDN.Close();
+
+            editor.visibleNodeList = visibleNodeList[0];
+            editor.ScrollEditorMiddle();
+            editor.redraw();
+
+            nodeEditor.dataNodeList = dataNodeList[0];
+            nodeEditor.Enabled = true;
+
+            updateMenu(true);
+        }
+
+        private void loadXmlFiles()
+        {
+            XmlDocument dataNodesXml = new XmlDocument();
+            XmlDocument visibleNodesXml = new XmlDocument();
+
+            dataNodesWorldOffsets = new int[8];
+            visibleNodesWorldOffsets = new int[8];
+            nodesPerWorld = new int[8];
+
+            //Visible Nodes XML
+            if (File.Exists("Supporting Files/visibleNodes.xml") == false)
+            {
+                MessageBox.Show("The VisibleNodes.xml File is missing!", "Error");
+                Environment.Exit(0);
+            }
+            else
+            {
+                visibleNodesXml.Load("Supporting Files/VisibleNodes.xml");
+                XmlNodeList visibleNodesXmlList = visibleNodesXml.GetElementsByTagName("World");
+                for (int i = 0; i < 8; i++)
+                {
+                    visibleNodesWorldOffsets[i] = int.Parse(visibleNodesXmlList[i].InnerText.Remove(0, 2), System.Globalization.NumberStyles.HexNumber);
+                }
+            }
+
+            //Data Nodes XML
+            if (File.Exists("Supporting Files/DataNodes.xml") == false)
+            {
+                MessageBox.Show("The DataNodes.xml File is missing!", "Error");
+                Environment.Exit(0);
+            }
+            else
+            {
+                dataNodesXml.Load("Supporting Files/DataNodes.xml");
+                XmlNodeList dataNodesXmlList = dataNodesXml.GetElementsByTagName("World");
+                for (int i = 0; i < 8; i++)
+                {
+                    dataNodesWorldOffsets[i] = int.Parse(dataNodesXmlList[i].InnerText.Remove(0, 2), System.Globalization.NumberStyles.HexNumber);
+                    nodesPerWorld[i] = int.Parse(dataNodesXmlList[i].Attributes["nodes"].Value);
+                }
+            }
+        }
+
+        private void writeOverlay()
+        {
+            //Write visible Nodes
+            BinaryWriter bwVN = new BinaryWriter(new FileStream("Backup/overlay9_8.tmp", FileMode.Open));
+            for (int i = 0; i < 8; i++)
+            {
+                bwVN.BaseStream.Position = visibleNodesWorldOffsets[i];
+                for (int j = 0; j < nodesPerWorld[i]; j++)
+                {
+                    bwVN.Write((byte)visibleNodeList[i][j].p1);
+                    bwVN.Write((byte)visibleNodeList[i][j].p2);
+                    bwVN.Write((byte)visibleNodeList[i][j].p3);
+                    bwVN.Write((byte)visibleNodeList[i][j].p4);
+                    bwVN.Write((byte)visibleNodeList[i][j].sp1);
+                    bwVN.Write((byte)visibleNodeList[i][j].sp2);
+                    bwVN.Write((byte)visibleNodeList[i][j].sp3);
+                    bwVN.Write((byte)visibleNodeList[i][j].sp4);
+                    bwVN.Write((Int16)visibleNodeList[i][j].c);
+                    bwVN.Write((Int16)visibleNodeList[i][j].sc);
+                    bwVN.Write(visibleNodeList[i][j].posX);
+                    bwVN.Write(visibleNodeList[i][j].posZ);
+                    bwVN.Write(visibleNodeList[i][j].posY);
+                }
+            }
+            bwVN.Close();
+
+            //Write data Nodes
+            BinaryWriter bwDN = new BinaryWriter(new FileStream("Backup/overlay9_8.tmp", FileMode.Open));
+            for (int i = 0; i < 8; i++)
+            {
+                bwDN.BaseStream.Position = dataNodesWorldOffsets[i];
+                for (int j = 0; j < nodesPerWorld[i]; j++)
+                {
+                    bwDN.BaseStream.Position += 4;
+                    bwDN.Write((byte)dataNodeList[i][j].area);
+                    bwDN.BaseStream.Position += 2;
+                    bwDN.Write((byte)dataNodeList[i][j].icon);
+                    bwDN.Write((byte)dataNodeList[i][j].settings);
+                    bwDN.BaseStream.Position += 3;
+                }
+            }
+            bwDN.Close();
+        }
+
+        int oldWorldIndex;
+        private void worldComboBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (visibleNodeList != null)
+            {
+                visibleNodeList[oldWorldIndex] = editor.visibleNodeList;
+                dataNodeList[oldWorldIndex] = nodeEditor.dataNodeList;
+                editor.visibleNodeList = visibleNodeList[worldComboBox.SelectedIndex];
+                nodeEditor.dataNodeList = dataNodeList[worldComboBox.SelectedIndex];
+                editor.nodeSelected = false;
+                editor.redraw();
+                editor.ScrollEditorMiddle();
+                nodeEditor.nodeChanged(0, false);
+                oldWorldIndex = worldComboBox.SelectedIndex;
+            }
+            
+        }
+
+        private void updateMenu(bool load)
+        {
+            worldComboBox.Enabled = true;
+            if (worldComboBox.SelectedIndex == -1) worldComboBox.SelectedIndex = 0;
+            saveToolStripMenuItem.Enabled = true;
+            saveAsToolStripMenuItem.Enabled = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!nodeEditor.Enabled) Environment.Exit(0);
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show("There are unsaved changes.\nIf you close the editor without saving, you will lose them.\nDo you want to save?", "NSMB Worldmap Editor", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button3);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    save(false);
+                    Environment.Exit(0);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            save(false);
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            save(true);
+        }
+
+        private void save(bool saveTo)
+        {
+            visibleNodeList[oldWorldIndex] = editor.visibleNodeList;
+            dataNodeList[oldWorldIndex] = nodeEditor.dataNodeList;
+            writeOverlay();
+
+            if (!saveTo)
+            {
+                File.Copy("Backup/overlay9_8.tmp", overlayPath, true);
+            }
+            else
+            {
+                SaveFileDialog saveOverlayDialog = new SaveFileDialog();
+                saveOverlayDialog.InitialDirectory = "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}";
+                saveOverlayDialog.FileName = "overlay9_8.bin";
+                saveOverlayDialog.Filter = "Binary Files|*.bin";
+                saveOverlayDialog.Title = "Save overlay9_8";
+
+                DialogResult saveOverlayDialogResult = saveOverlayDialog.ShowDialog();
+                if (saveOverlayDialogResult == DialogResult.OK)
+                {
+                    overlayPath = saveOverlayDialog.FileName;
+                    File.Copy("Backup/overlay9_8.tmp", overlayPath, true);
+                }
+            }
+        }
+
+    }
+}
